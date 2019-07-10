9bf9b584ba346e4d555149f89c1a4c282bc1f99a
Gael gendron <gael.gendron@insa-rennes.fr>
2019-07-10 09:02:30 +0200

    added result extractor + test ranges

---
+++ include/learning/learners/sdflearner.h
@@ -0,0 +1,35 @@
+/**
+ * @file sdflearner.h
+ * @author Gaël Gendron (gael.gendron@insa-rennes.fr)
+ * @brief File containing the abstract class SdfLearner, used for computing positions using learner created with Preesm 
+ * @version 0.1
+ * @date 2019-06-11
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+
+
+#ifndef SDFLEARNER_H
+#define SDFLEARNER_H
+
+#include "devicelearner.h"
+#include "input.h"
+#include "output.h"
+
+
+/**
+ * @class SdfLearner
+ * @brief Class containing an interface linking to a learner using SDF and created with Preesm software
+ * 
+ */
+class SdfLearner : public DeviceLearner{
+
+    
+
+
+};
+
+
+#endif
--- src/builder/widowxbuilder.cpp
+++ src/builder/widowxbuilder.cpp
@@ -18,7 +18,7 @@ WidowXBuilder::~WidowXBuilder(){
 void WidowXBuilder::buildController(AbstractController& controller){
     controller.addMotor(1, "base ", base);
 	controller.addMotor(2, "shoulder", shoulder);
-	//controller.addMotor(3, "elbow", elbow);
+	controller.addMotor(3, "elbow", elbow);
 	//controller.addMotor(4, "wristAngle", wristAngle);
 	//controller.addMotor(5, "wristRotate", wristRotate);
 	//controller.addMotor(6, "gripper", gripper);
@@ -27,7 +27,7 @@ void WidowXBuilder::buildController(AbstractController& controller){
 void WidowXBuilder::buildConverter(Converter& converter){
     converter.addServo("base", rotZ, 0, 0, 125, 0, 0, M_PI);
 	converter.addServo("shoulder", rotX, 0, -49, 142, M_PI/2, 0, M_PI); // Add 90° because of the orientation of the elbow servomotor
-	//converter.addServo("elbow", rotX, 0, 0, 142);
+	converter.addServo("elbow", rotX, 0, 0, 142);
 	//converter.addServo("wristAngle", rotX, 0, 0, 74);
 	//converter.addServo("wristRotate", rotZ, 0, 0, 41);
 	//converter.addServo("gripper", rotZ, 0, 0, 40);
