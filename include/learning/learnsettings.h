8d3730a98fc1372bfdf8319ae311e86434429caa
Gael gendron <gael.gendron@insa-rennes.fr>
2019-08-21 08:07:14 +0200

    Normalized c++ learner reward

--- include/learning/learnsettings.h
+++ include/learning/learnsettings.h
@@ -14,11 +14,11 @@
 #define LEARNSETTINGS_H
 
 // Coefficient of target error (difference between the real output and the target output, to minimize) when computing error between input and output
-#define TARGET_COEFF 3.36
+#define TARGET_COEFF 8e-4
 // Coefficient of movement error (distance browsed by servomotors to reach target, to minimize) when computing error between input and output
-#define MOVEMENT_COEFF 0.032
+#define MOVEMENT_COEFF 3.467e-6
 // Coefficient of valid position error (returned if position is not valid)
-#define VALID_COEFF -2000
+#define VALID_COEFF -1
 
 // Margin of error allowing to stop learning if error is below the number
 #define LEARN_ERROR_MARGIN 0.05
