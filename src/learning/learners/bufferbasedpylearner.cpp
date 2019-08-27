8fbdbeaadf14c9d2e3ce53d16924a32bd48d69b4
Gael gendron <gael.gendron@insa-rennes.fr>
2019-08-27 11:01:38 +0200

    C++ multi coord set learning

--- src/learning/learners/bufferbasedpylearner.cpp
+++ src/learning/learners/bufferbasedpylearner.cpp
@@ -18,108 +18,114 @@ BufferBasedPyLearner::~BufferBasedPyLearner(){
 
 void BufferBasedPyLearner::learn(){
 
-    for(auto lsetPtr = learningSet->begin(); lsetPtr != learningSet->end(); lsetPtr++){ // Learn trajectory for each example from the learning set
+    auto lsetPtr = learningSet->begin(); // Learn trajectory for each example from the learning set
 
-        for(int nbIt = 0; nbIt < LEARN_NB_ITERATIONS; nbIt++){
-            std::cout << "Reset device position..." << std::endl;
-            device->goToBackhoe(); // Reset position
-            device->waitFeedback();
+    for(int nbIt = 0; nbIt < LEARN_NB_ITERATIONS; nbIt++){
 
-            bool stop = false;
-            int nbNullMove = 0;
-            std::vector<State*> saves;
+        lsetPtr++; // Switch learning example at each iteration
+        if(lsetPtr == learningSet->end()) lsetPtr = learningSet->begin();
 
-            // Attempts to reach target
-            for(int nbMove = 0; nbMove < LEARN_NB_MOVEMENTS; nbMove++){
-                std::cout << "Iteration " << nbIt << " - Move " << nbMove << " : " << std::endl;
+        std::cout << "Reset device position..." << std::endl;
+        device->goToBackhoe(); // Reset position
+        device->waitFeedback();
 
+        bool stop = false;
+        int nbNullMove = 0;
+        std::vector<State*> saves;
 
-                std::vector<uint16_t> fullInput(lsetPtr->first->getInput()); // Create input of the DNN, add the target coordinates
-                auto state = DeviceLearner::getDeviceState(); // Get state of servomotors
-
-                for(auto ptr = state.cbegin(); ptr < state.cend(); ptr++) {
-                    fullInput.insert(fullInput.end(), ptr->begin(), ptr->end()); // Add the current state of the servomotors to the input
-                }
-                
-                std::cout << "Computing output..." << std::endl;
-                auto output = pyCompute(fullInput); // Computation of output
-                auto scaledOutput = formatOutput(output); // Format output for shipment to device
-
-                std::cout << "Scaled output : [";
-                for(auto ptr = scaledOutput.cbegin(); ptr < scaledOutput.cend(); ptr++) std::cout << *ptr << ", ";
-                std::cout << "]" << std::endl;
+        // Attempts to reach target
+        for(int nbMove = 0; nbMove < LEARN_NB_MOVEMENTS; nbMove++){
+            std::cout << "Iteration " << nbIt << " - Move " << nbMove << " : " << std::endl;
 
 
-                auto reward = computeReward(lsetPtr->first->getInput(), scaledOutput); // Computation of reward
-                std::cout << "Reward : " << reward << std::endl;
+            std::vector<uint16_t> fullInput(lsetPtr->first->getInput()); // Create input of the DNN, add the target coordinates
+            auto state = DeviceLearner::getDeviceState(); // Get state of servomotors
 
-                std::vector<double> rewardVector = {reward};
-                saves.push_back(new State(fullInput, output, rewardVector));  // Save state
-                
-                
-                if(reward > VALID_COEFF){ // If position is valid (within range)
-                    try{
-                        device->setPosition(scaledOutput); // Update position
-                        device->waitFeedback();
+            for(auto ptr = state.cbegin(); ptr < state.cend(); ptr++) {
+                fullInput.insert(fullInput.end(), ptr->begin(), ptr->end()); // Add the current state of the servomotors to the input
+            }
+            
+            std::cout << "Computing output..." << std::endl;
+            auto output = pyCompute(fullInput); // Computation of output
+            auto scaledOutput = formatOutput(output); // Format output for shipment to device
 
-                        std::cout << "Updating position..." << std::endl;
-                    }catch(OutOfRangeError e){
-                        std::cout << e.what() << std::endl;
-                        nbNullMove++;
-                    }
-                }else{
-                    std::cout << "Error too important : movement not allowed" << std::endl;
+            std::cout << "Scaled output : [";
+            for(auto ptr = scaledOutput.cbegin(); ptr < scaledOutput.cend(); ptr++) std::cout << *ptr << ", ";
+            std::cout << "]" << std::endl;
+
+
+            auto reward = computeReward(lsetPtr->first->getInput(), scaledOutput); // Computation of reward
+            std::cout << "Reward : " << reward << std::endl;
+
+            std::vector<double> rewardVector = {reward};
+            saves.push_back(new State(fullInput, output, rewardVector));  // Save state
+            
+            
+            if(reward > VALID_COEFF){ // If position is valid (within range)
+                try{
+                    device->setPosition(scaledOutput); // Update position
+                    device->waitFeedback();
+
+                    std::cout << "Updating position..." << std::endl;
+                }catch(OutOfRangeError e){
+                    std::cout << e.what() << std::endl;
                     nbNullMove++;
                 }
-
-                auto newPosition = device->getPosition(); // Display new position
-                std::cout << "Current position : ";
-                for(auto ptr = newPosition.cbegin(); ptr < newPosition.cend(); ptr++) {
-                    std::cout << *ptr << " ";
-                }
-                std::cout << std::endl;
-
-                auto newCoords = verifier->computeServoToCoord(newPosition)->getCoord(); // Display corresponding new coordinates
-                std::cout << "Current coordinates : ";
-                for(auto ptr = newCoords.cbegin(); ptr < newCoords.cend(); ptr++) {
-                    std::cout << *ptr << " ";
-                }
-                std::cout << std::endl;
-                
-                /*
-                if(abs(reward) < LEARN_ERROR_MARGIN) {  // Stop moving if error is within threshold
-                    std::cout << "reward value : " << reward << " smaller than " << LEARN_ERROR_MARGIN << ". End of learning..." << std::endl;
-                    stop = true;
-                    break;
-                }
-                //*/
-
-                if(nbNullMove > MAX_NULL_MOVE) break; // Stop iteration if too many null movements
+            }else{
+                std::cout << "Error too important : movement not allowed" << std::endl;
+                nbNullMove++;
             }
 
-            if(stop) break;
+            auto newPosition = device->getPosition(); // Display new position
+            std::cout << "Current position : ";
+            for(auto ptr = newPosition.cbegin(); ptr < newPosition.cend(); ptr++) {
+                std::cout << *ptr << " ";
+            }
+            std::cout << std::endl;
+
+            auto newCoords = verifier->computeServoToCoord(newPosition)->getCoord(); // Display corresponding new coordinates
+            std::cout << "Current coordinates : ";
+            for(auto ptr = newCoords.cbegin(); ptr < newCoords.cend(); ptr++) {
+                std::cout << *ptr << " ";
+            }
+            std::cout << std::endl;
             
-            // Update learner from its experience
-            std::cout << "Backtracking reward to learner..." << std::endl;
-            int i = 0;
-            for(auto ptr = saves.rbegin(); ptr < saves.rend(); ptr++){
-                
-                std::vector<uint16_t> nextInput = (*(ptr+((ptr+1) != saves.rend())))->getInput(); // Send input of the next state reached with the given output (if no next input, is current input)
-                pyLearn((*ptr)->getInput(), (*ptr)->getOutput(), (*ptr)->getReward(), nextInput, std::pow(DECREASING_REWARD, i)); // Decrease value of reward as the state is closer to initial state
-
-                
-                delete *ptr;
-                i++;
+            /*
+            if(abs(reward) < LEARN_ERROR_MARGIN) {  // Stop moving if error is within threshold
+                std::cout << "reward value : " << reward << " smaller than " << LEARN_ERROR_MARGIN << ". End of learning..." << std::endl;
+                stop = true;
+                break;
             }
-            std::cout << "Executed " << i << " updates..." << std::endl;
+            //*/
+
+            if(nbNullMove > MAX_NULL_MOVE) break; // Stop iteration if too many null movements
         }
+
+        if(stop) break;
+        
+        // Update learner from its experience
+        std::cout << "Backtracking reward to learner..." << std::endl;
+        int i = 0;
+        for(auto ptr = saves.rbegin(); ptr < saves.rend(); ptr++){
+            
+            std::vector<uint16_t> nextInput = (*(ptr+((ptr+1) != saves.rend())))->getInput(); // Send input of the next state reached with the given output (if no next input, is current input)
+            pyLearn((*ptr)->getInput(), (*ptr)->getOutput(), (*ptr)->getReward(), nextInput, std::pow(DECREASING_REWARD, i)); // Decrease value of reward as the state is closer to initial state
+
+            
+            delete *ptr;
+            i++;
+        }
+        std::cout << "Executed " << i << " updates..." << std::endl;
+    }
+
+    for(auto ptr = learningSet->begin(); ptr != learningSet->end(); ptr++){ // Produce output for each example from the learning set
 
         std::cout << "Reset device position..." << std::endl;
         device->goToBackhoe(); // Reset position
         device->waitFeedback();
         
-        delete lsetPtr->second;
-        lsetPtr->second = produce(*(lsetPtr->first));
+        delete ptr->second;
+        ptr->second = produce(*(ptr->first));
 
     }
 }
